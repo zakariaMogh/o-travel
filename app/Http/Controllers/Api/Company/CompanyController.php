c7f131b5b062073bbb36b3121d14919f3007cc04
KaraAliAbdElbasset <karaliabdelbasset@outlook.com>
2021-12-14 20:10:54 +0100

    add companies rouet to company api

---
+++ app/Http/Controllers/Api/Company/CompanyController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api\User;
+
+use App\Contracts\CompanyContract;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CompanyResource;
+use Illuminate\Http\Request;
+
+class CompanyController extends Controller
+{
+    protected $company;
+
+    public function __construct(CompanyContract $company)
+    {
+        $this->company = $company;
+    }
+
+    public function index()
+    {
+        $companies = $this->company->setPerPage(15)->findByFilter();
+
+        return CompanyResource::collection($companies);
+    }
+
+    public function show($id)
+    {
+        $company = $this->company->findOneById($id);
+
+        return new CompanyResource($company);
+    }
+}
